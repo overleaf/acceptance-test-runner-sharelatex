087450c80189bc72c2b2d5248ad5b7eb4f3388b7
Hayden Faulds <fauldsh@gmail.com>
2017-11-20 11:41:02 +0000

    wait for mongo to be up instead of waiting 30s

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,7 +3,23 @@
 /usr/bin/redis-server /etc/redis/redis.conf
 service mongodb start
 
-sleep 30
+count=1
+max_wait=30
 
-cd /app
-exec $1
+while [ $count -le $max_wait ]
+do
+  if nc -z localhost 27017
+  then
+    echo "   Connected to mongo"
+    cd /app
+    exec "$@"
+    exit 0
+  fi
+
+  sleep 1
+  echo "."
+  count=$((count+1))
+done
+
+echo "   Failed to connect to mongo within $max_wait seconds"
+exit 1
